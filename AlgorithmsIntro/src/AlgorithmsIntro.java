26366214f7b5cda86f662d6dfaee2f17d6db36ae
erikdkarlsson <46487081+erikdkarlsson@users.noreply.github.com>
2021-12-27 07:08:52 +0100

    Initial commit

---
+++ AlgorithmsIntro/src/AlgorithmsIntro.java
@@ -0,0 +1,42 @@
+import edu.princeton.cs.algs4.In;
+//import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+public class AlgorithmsIntro
+{
+   public static void main(String[] args)
+   {
+      //Init all ints, for comparison
+      int noOne = 0;
+      int noTwo = 0;
+      int noThree = 0;
+      
+      //Takes three numbers as arguments in try block, to make sure user entered correct cla
+      try
+      {
+      noOne = Integer.parseInt(args[0]);
+      noTwo = Integer.parseInt(args[1]);
+      noThree = Integer.parseInt(args[2]);
+      }
+      //If too few or incorrect input
+      catch (NumberFormatException | ArrayIndexOutOfBoundsException e)
+      {
+         System.out.println("Please write exactly three numbers as arguments: java AlgorithmsIntro no1 no2 no3");
+         System.exit(1);
+      }
+      //Check if all three inputs are equal
+      if (noOne == noTwo && noTwo == noThree)
+      {
+         System.out.println("Equal");
+      }
+      else
+      {
+         System.out.println("Not equal");
+      }
+           
+      
+   }
+}
